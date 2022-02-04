28c0c49e43f0286cd23c9de38c0be8a4321dfaf9
Nádia Mizael <nadia.mizael@gmail.com>
2022-02-04 17:02:01 -0300

    atualizações database

--- LEGO.sql
+++ LEGO.sql
@@ -1,9 +1,39 @@
 CREATE DATABASE `LEGO`;
+USE `LEGO`;
 
-CREATE TABLE `sets` (
-`set_num` VARCHAR(20),
-`name`  VARCHAR(200),
-`year` SMALLINT,
-`theme_id` SMALLINT,
-`num_parts` SMALLINT
-)
+SELECT *FROM `inventory_parts`;
+SELECT *FROM `sets`;
+SELECT *FROM `themes`;
+
+
+/*TESTES
+select inventory_id,  count(quantity)from inventory_parts;
+
+select `name` from part_categories order by `name`;
+
+select name , part_num, part_categories.name from parts 
+inner join inventory_parts 
+inner join part_categories 
+using(part_num) on parts.part_cat_id = part_categories.id 
+group by part_num;
+
+select `name`, `num_parts`, `year` 
+from sets 
+order by num_parts 
+desc limit 5;
+
+select max(num_parts), `name`, `year` 
+from sets;
+
+select themes.`name`, sets.`year` 
+from themes 
+inner join sets 
+on themes.id = sets.theme_id;
+
+select sets.name, count(sets.theme_id) as total from sets 
+inner join themes on themes.id= sets.theme_id where themes.name like '%batman%'
+group by sets.name having count(sets.theme_id) order by  total desc;
+
+*/
+
+ 
